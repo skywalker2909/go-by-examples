15ba59b5c70783c8849f1a43e6787d9f900790a5
Neville Bokdawalla <nevillekb@gmail.com>
2026-01-12 02:08:54 +0530

    feat: simple web server

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Learn golang by examples
 
 ## Tutorials
 - [Hello world](./tutorials/helloworld/main.go) - Getting started with a small demo.
-- [Command line arguments](./tutorials/commandline/main.go) - How to take inputs from command line arguments
+- [Command line arguments](./tutorials/commandline/main.go) - How to take inputs from command line arguments.
+- [Web server](./tutorials/webserver/main.go) - Implement a simple HTTP web server.
---
+++ tutorials/webserver/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	/*
+		log package provides functions for logging information in the
+		form of errors, warnings, debug and info etc.
+	*/
+	"log"
+	/*
+		net/http package provides http client and server implementations
+		that can be used to create an http client to send http requests
+		and an http server to serve http requests.
+	*/
+	"net/http"
+)
+
+func main() {
+	/*
+		Register an http request handler function for handling
+		all incoming requests that begin with a '/' in the URL.
+
+		For example: /. /help, /about-me etc. in
+		http://localhost:8000/
+		http://localhost:8000/help
+		http://localhost:8000/about-me
+	*/
+	http.HandleFunc("/", handler)
+
+	/*
+		'http.ListenAndServe' starts an http server and listens for
+		incoming requests on port number 8000
+
+		It returns an error on failure to listen and serve which is
+		passed on to 'log.Fatal' as a parameter to log and exit.
+	*/
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
